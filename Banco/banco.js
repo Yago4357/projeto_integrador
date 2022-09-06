8b6e456f87413e2fc4beef2f7b6e62a0ffa6bba3
Leandro Medeiros <104951666+Leandromf12@users.noreply.github.com>
2022-09-06 20:40:29 -0300

    Update banco.js

--- Banco/banco.js
+++ Banco/banco.js
@@ -3,7 +3,7 @@ async function connect(){
     console.log("Conectou!");
     const mysql = require("mysql2/promise");
 
-    const connection = await mysql.createConnection("mysql://root:28047814lw@localhost:3306/banco");
+    const connection = await mysql.createConnection("mysql://root:aluno@localhost:3306/banco");
     console.log("Conectou no MySQL!");
     global.connection = connection;
     return connection;
