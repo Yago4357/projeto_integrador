1b1c6f7c6eb81bddd89cefeeb264599a9eb72200
Yago Lima Bortolini <yagolimabortolini@gmail.com>
2022-10-26 21:58:43 -0300

    bd arrumado

--- Banco/banco_rvenda.sql
+++ Banco/banco_rvenda.sql
@@ -27,7 +27,7 @@ CREATE TABLE `rvenda` (
   `comV` double NOT NULL,
   `qtdV` int NOT NULL,
   `vF` double NOT NULL,
-  `Cliente` int NOT NULL,
+  `Cliente` varchar(11) NOT NULL,
   PRIMARY KEY (`codV`)
 ) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
