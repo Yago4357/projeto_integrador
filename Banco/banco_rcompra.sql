64d8aec5513ab8376c8948417334c946c0ad403a
Leandro Medeiros <leandro_medeiros2003@hotmail.com>
2022-11-24 07:58:11 -0300

    b

--- Banco/banco_rcompra.sql
+++ Banco/banco_rcompra.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: banco
 -- ------------------------------------------------------
--- Server version	8.0.30
+-- Server version	5.7.20-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,12 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `rcompra`
+--
+
+DROP TABLE IF EXISTS `rcompra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rcompra` (
+  `idC` int(11) NOT NULL AUTO_INCREMENT,
+  `Produto` varchar(49) NOT NULL,
+  `qtdC` int(11) NOT NULL,
+  `vC` double DEFAULT NULL,
+  `dt` datetime DEFAULT NULL,
+  `forn` varchar(49) NOT NULL,
+  PRIMARY KEY (`idC`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `rcompra`
 --
 
 LOCK TABLES `rcompra` WRITE;
 /*!40000 ALTER TABLE `rcompra` DISABLE KEYS */;
-INSERT INTO `rcompra` VALUES (1,1,'Cadeira',50,NULL,'Pichau'),(2,1,'Cadeira',50,NULL,'Pichau');
+INSERT INTO `rcompra` VALUES (1,'Mouse',12,160,NULL,'Red dragon');
 /*!40000 ALTER TABLE `rcompra` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -34,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-18 14:05:00
+-- Dump completed on 2022-11-24  7:57:17
